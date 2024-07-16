48d4df74bc94f45251c531a73c85c97a21442856
emillymoitinho <165393316+emillymoitinho@users.noreply.github.com>
2024-07-16 10:54:58 -0300

    javascript particularities

---
+++ 3_particularidades_javascript/js/scripts.js
@@ -0,0 +1,23 @@
+console.log('Primeiro');
+
+console.log('Último');
+
+console.log("Mais um console.log");
+
+var a = 1;
+
+a = 'texte';
+
+console.log(a);
+
+// Este é um comentáro de uma linha!
+
+/* 
+Este
+é
+um
+comentário
+de
+multiplas
+linhas
+*/
