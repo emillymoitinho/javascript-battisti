dc7436b4f111d4292e3cb7a97c6949933ab4c26d
emillymoitinho <165393316+emillymoitinho@users.noreply.github.com>
2024-07-18 09:53:27 -0300

    number

---
+++ 5_number/js/scripts.js
@@ -0,0 +1,28 @@
+
+var numero = 5;
+
+console.log(numero);
+console.log(typeof numero);
+
+var float = 5.32;
+
+console.log(float);
+console.log(typeof float);
+
+var textoComNumero = '523';
+
+/* 
+
+Esssa variável é considerada uma string, porque está entre aspas ''!!
+
+Devemos tomar cuidado quando vou fazer uma variável com textos e números!! 
+
+*/ 
+
+console.log(textoComNumero);
+console.log(typeof textoComNumero);
+
+console.log(NaN);
+console.log(typeof NaN);
+console.log(typeof +Infinity);
+console.log(typeof -Infinity);
