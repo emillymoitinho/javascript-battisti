c331e3e55416c70c0e6d492edfcb0cecf777363f
emillymoitinho <165393316+emillymoitinho@users.noreply.github.com>
2024-07-19 09:39:53 -0300

    boolean finished file

---
+++ 7_boolean/js/scripts.js
@@ -0,0 +1,26 @@
+var verdadeiro = true;
+
+console.log(verdadeiro);
+console.log(typeof verdadeiro);
+
+console.log(typeof true);
+console.log(typeof false);
+
+var falso = false;
+
+console.log(falso);
+console.log(typeof falso);
+
+/* 
+
+==== Palavras Reservadas no JS ====
+
+Em JavaScript, existem algumas palavras reservadas que não podem ser usadas como identificadores (como nomes de variáveis, funções, etc.) 
+Porque são parte da linguagem.
+
+ex: false, true, function, var
+
+*/
+
+
+
