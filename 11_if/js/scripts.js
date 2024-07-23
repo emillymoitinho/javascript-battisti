026e18c7a5b3911279b761e02fac6ec64c46b78c
emillymoitinho <165393316+emillymoitinho@users.noreply.github.com>
2024-07-23 11:02:39 -0300

    finished if

---
+++ 11_if/js/scripts.js
@@ -0,0 +1,13 @@
+var idade = 16;
+
+console.log("Antes do if");
+
+if(idade > 18) {
+    console.log("Pode fazer a carteira de habilitação");
+}
+
+if(idade > 15) {
+    console.log("Precisa esperar 3 anos ainda")
+}
+
+console.log("Depois do if");
