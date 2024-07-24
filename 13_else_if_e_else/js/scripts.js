59aee8fbff4bd7b48265df80613c156120abf102
emillymoitinho <165393316+emillymoitinho@users.noreply.github.com>
2024-07-24 11:07:46 -0300

    finished else if e else

---
+++ 13_else_if_e_else/js/scripts.js
@@ -0,0 +1,19 @@
+var nome = "Emilly";
+
+if(nome == "Moitinho") {
+    console.log("O nome dela é Moitinho");
+} else if( nome == "Emilly") {
+    console.log("O nome é Emilly")
+} else if(nome == "Soares") {
+    console.log("O nome é Soares")
+} else {
+    console.log("Ela possui outro nome!");
+}
+
+var idade = 13;
+
+if(idade >= 18) {
+    console.log("Ele pode entrar na festa!")
+} else if(idade <= 17) {
+    console.log("Ele só pode entrar com autorização")
+}
