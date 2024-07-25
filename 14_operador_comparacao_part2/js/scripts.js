8bfd2726a494089584bfc84af1489bfb3bdd10d2
emillymoitinho <165393316+emillymoitinho@users.noreply.github.com>
2024-07-25 10:09:28 -0300

    finished op com - part2

---
+++ 14_operador_comparacao_part2/js/scripts.js
@@ -0,0 +1,16 @@
+/* 
+OPERADOR === 
+
+O operador estrito de igualdade (===) compara não apenas os valores, mas também os tipos dos operandos. 
+
+*/
+var numero = "5";
+console.log(typeof numero)
+
+if( numero === 5) {
+    console.log("O número é 5")
+}
+
+if(numero !== 5) {
+    console.log("Não é o número 5 do tipo number");
+} 
