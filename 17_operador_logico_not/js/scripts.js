1cea542500bf56f0ca066ac64eb4e132995af54f
MedMuniz <165393316+emillymoitinho@users.noreply.github.com>
2024-07-26 10:45:26 -0300

    finished op logico: not

---
+++ 17_operador_logico_not/js/scripts.js
@@ -0,0 +1,19 @@
+if(!true) {
+    console.log("Passou")
+}
+
+if(!false) {
+    console.log("Passou2")
+}
+
+var nome = "Emilly";
+
+if(nome == "Emilly") {
+    console.log("Ok");
+}
+
+var nome = "Emilly";
+
+if(!(nome == "Moitinho")) {
+    console.log("Ok2");
+}
