7089f20309a33e5586d7249612765da1f033122d
emillymoitinho <165393316+emillymoitinho@users.noreply.github.com>
2024-07-29 11:02:12 -0300

    finished estrutura de repetição: for

---
+++ 20_estrutura_de_repeticao_for/js/scripts.js
@@ -0,0 +1,16 @@
+for(var i = 0; i < 10; i++) {
+
+    console.log("Repetindo for: " + i);
+
+}
+
+var arr = [1,2,3,4];
+
+for(var j = 0; j < arr.length; j++ ) {
+    console.log(arr[j]);
+}
+// arr.length é o número de elementos no array, que é 4 neste caso.
+
+for(var x=5; x < 100; x *= 3) {
+    console.log(x);
+}
